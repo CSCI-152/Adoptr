b28aff8cb4dece8f3198322d9228bcf982d3660e
Julian559 <56282131+Julian559@users.noreply.github.com>
2021-04-10 10:09:33 -0700

    Updated user class to include role (default = user)

--- app/src/main/java/com/example/adoptrapp/ClassUser.kt
+++ app/src/main/java/com/example/adoptrapp/ClassUser.kt
@@ -2,6 +2,7 @@ package com.example.adoptrapp
 
 data class ClassUser(
     var fullName: String? = null,
-    var email: String? = null
+    var email: String? = null,
+    var role: String? = "user"
 )
 
